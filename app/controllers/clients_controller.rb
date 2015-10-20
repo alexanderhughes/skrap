a2031bbacfdf815121c7777c7feddac4847667a8
Alexander Hughes <ahughes1231@gmail.com>
2015-10-19 23:14:49 -0400

    Add show page

--- app/controllers/clients_controller.rb
+++ app/controllers/clients_controller.rb
@@ -1,6 +1,15 @@
 class ClientsController < ApplicationController
+  def create
+    @client = Client.create
+    redirect_to client_path(@client.last)
+  end
+  
   def new
     @client = Client.new
+  end
+  
+  def show
+    @client = Client.all
   end
   
   protect_from_forgery with: :exception
